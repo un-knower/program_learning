c9ddbe0a6aaff632ac01c2f1e65ad7e86dd76e51
bjwangguangliang <bjwangguangliang@corp.netease.com>
2018-07-06 17:36:22 +0800

    redis bloomfilter

--- src/main/java/demo/redis/bloomfilter/App.java
+++ src/main/java/demo/redis/bloomfilter/App.java
@@ -6,11 +6,15 @@ import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 import com.github.wxisme.bloomfilter.common.BloomFilter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 唯一需要注意的是redis的bitmap只支持2^32大小，对应到内存也就是512MB,数组的下标最大只能是2^32-1。
  * 不过这个限制我们可以通过构建多个redis的bitmap通过hash取模的方式分散一下即可。
  */
 public class App {
+    private static final int NUM_OF_BLOOM_FILTER = 2;
     public static void main(String args[]) throws InterruptedException {
         //Jedis jedis , String name
 
@@ -18,88 +22,90 @@ public class App {
         config.setTestOnBorrow(false);
         JedisPool pool = new JedisPool(config, "192.168.1.160", 6379, 100000);
         Jedis jedis = pool.getResource();
-        RedisBitSet redisBitSet0 = new RedisBitSet(jedis, "test");
-        RedisBitSet redisBitSet1 = new RedisBitSet(jedis, "test2");
+        RedisBitSet redisBitSet0 = new RedisBitSet(jedis, "test");   // bitmap key为test，一个大约占用redis 256MB
+        RedisBitSet redisBitSet1 = new RedisBitSet(jedis, "test2");  // bitmap key为test
 
 
         double falsePositiveProbability = 0.000001d;
-        int expectedNumberOElements = Integer.MAX_VALUE;
+        int expectedNumberOElements = Integer.MAX_VALUE;  // 预存放元素个数
         BloomFilter bloomFilter0 = new BloomFilter(falsePositiveProbability, expectedNumberOElements);
         bloomFilter0.bind(redisBitSet0);
         BloomFilter bloomFilter1 = new BloomFilter(falsePositiveProbability, expectedNumberOElements);
         bloomFilter1.bind(redisBitSet1);
 
-        for (int i=0;i<3;i++) {
-            if (i%2 ==0) {  // 因为是两个redis bitmap
-                System.out.println(i+":"+bloomFilter0.contains(i));
-            } else {
-                System.out.println(i+":"+bloomFilter1.contains(i));
 
+        List<String> deviceIds = new ArrayList<>();
+        deviceIds.add("dev1");
+        deviceIds.add("dev2");
+        deviceIds.add("dev3");
+        deviceIds.add("dev4");
+        deviceIds.add("dev5");
+        deviceIds.add("dev1");
+        deviceIds.add("dev2");
+        deviceIds.add("dev3");
+        deviceIds.add("dev4");
+        deviceIds.add("dev5");
+        deviceIds.add("dev6");
+        deviceIds.add("dev7");
+
+        for (String devId : deviceIds) {
+            switch ( Math.abs(devId.hashCode()) % NUM_OF_BLOOM_FILTER) {  // 一定要加 Math.abs( xx.hashCode() ) 因为xx.hashCode()有可能为负数
+                case 0 : {
+                    boolean contains = bloomFilter0.contains(devId);
+                    System.out.println("contain "+devId+" ? "+contains);
+                    if (!contains) {
+                        System.out.println("add "+devId);
+                        bloomFilter0.add(devId);
+                    }
+                    bloomFilter0.count();
+                    break;
+                }
+                case 1 : {
+                    boolean contains = bloomFilter1.contains(devId);
+                    System.out.println("contain "+devId+" ? "+contains);
+                    if (!contains) {
+                        System.out.println("add "+devId);
+                        bloomFilter1.add(devId);
+                    }
+                    break;
+                }
+                default: {
+                    boolean contains = bloomFilter0.contains(devId);
+                    System.out.println("contain "+devId+" ? "+contains);
+                    if (!contains) {
+                        System.out.println("add "+devId);
+                        bloomFilter0.add(devId);
+                    }
+                    bloomFilter0.add(devId);
+                }
             }
+
+
         }
 
-//
-//        for (int i=0;i<10000;i++) {
-//
-//            //jedis = pool.getResource();
-//
-//            System.out.println("add "+i);
-//            if (i%2==0) {
-//                bloomFilter0.add(i);
-//            } else {
-//                bloomFilter1.add(i);
-//            }
-//            pool.returnResource(jedis);
-//            //System.out.println(jedis == null);
-//
-//        }
-
-//        for (int i=0;i<10000;i++) {
-//            jedis = pool.getResource();
-//            System.out.println("check "+i);
-//            if (i%2==0) {
-//                boolean trueContain = bloomFilter0.contains(i);
-//                if (!trueContain) {
-//                    System.out.println("should contain");
-//                }
-//            } else {
-//                boolean trueContain = bloomFilter1.contains(i);
-//                if (!trueContain) {
-//                    System.out.println("should contain");
-//                }
-//            }
-//            pool.returnResource(jedis);
-//        }
-//
-//        for (int i=10000;i<100000;i++) {
-//            jedis = pool.getResource();
-//            System.out.println("check "+i);
-//            if (i%2==0) {
-//                boolean trueContain = bloomFilter0.contains(i);
-//                if (trueContain) {
-//                    System.out.println("should not contain");
-//                }
-//            } else {
-//                boolean trueContain = bloomFilter1.contains(i);
-//                if (trueContain) {
-//                    System.out.println("should not contain");
-//                }
-//            }
-//            pool.returnResource(jedis);
-//        }
-//
-//        System.out.println("sleeping...");
-//        Thread.sleep(200000);
-//
-//        for (int i=9990;i<20000;i++) {
-//            if (i%2==0) {
-//                boolean trueContain = bloomFilter0.contains(i);
-//                System.out.println(trueContain);
-//            } else {
-//                boolean trueContain = bloomFilter1.contains(i);
-//                System.out.println(trueContain);
-//            }
-//        }
     }
 
 }
+
+
+/*
+        contain dev1 ? false
+        add dev1
+        contain dev2 ? false
+        add dev2
+        contain dev3 ? false
+        add dev3
+        contain dev4 ? false
+        add dev4
+        contain dev5 ? false
+        add dev5
+        contain dev1 ? true
+        contain dev2 ? true
+        contain dev3 ? true
+        contain dev4 ? true
+        contain dev5 ? true
+        contain dev6 ? false
+        add dev6
+        contain dev7 ? false
+        add dev7
+*/
--- src/main/java/demo/redis/bloomfilter/App2.java
+++ src/main/java/demo/redis/bloomfilter/App2.java
@@ -1,6 +1,12 @@
 package demo.redis.bloomfilter;
 
 
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+import java.util.BitSet;
+
 public class App2 {
     public static void main(String args[]) {
         System.out.println(Math.log(5)/Math.log(10));
@@ -16,6 +22,18 @@ public class App2 {
         for (int i=0;i<10;i++) {
             System.out.println(i%2);
         }
+
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setTestOnBorrow(false);
+        JedisPool pool = new JedisPool(config, "192.168.1.160", 6379, 100000);
+        Jedis jedis = pool.getResource();
+
+
+
+        BitSet initBitSet1 = new BitSet(55);
+        BitSet initBitSet2 = new BitSet(129);
+
+        // https://www.cnblogs.com/xupengzhang/p/7966755.html
     }
 
 }
